5aafab476c4fcf009d4289178b0ccd476e5723e7
Xarizz <76038882+Xarizz@users.noreply.github.com>
2023-04-12 22:24:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 const SECRET_KEY = '33595c822eb6d52452e653a39aa109a185642e7d102cf2f300669911a3f5be3a';
 
-
+const steamAccs = []
 function validateBearerToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
@@ -24,25 +24,36 @@ function validateBearerToken(req, res, next) {
         next();
     });
 }
+app.post('/gensteam', validateBearerToken, (req, res) => {
+    const { username, password } = req.query;
+  
+    if (!username || !password) {
+      return res.status(400).json({ error: 'Bad Request: Missing username or password' });
+    }
+  
+    steamAccs.push({ username, password });
+    res.status(201).json({ message: 'Credentials added to the array' });
+  });
 
+app.get('/steam', (req, res) => {
+    if (steamAccs.length == 0) {
+        return res.status(404).json({ error: 'accounts generator reached!' });
+    }
 
-app.get('/protected', validateBearerToken, (req, res) => {
-    res.json({ message: 'You have access to the protected resource!' });
+    const firstCredential = steamAccs.shift();
+    res.json(firstCredential);
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-
-
 // const jwt = require('jsonwebtoken');
 // const SECRET_KEY = '33595c822eb6d52452e653a39aa109a185642e7d102cf2f300669911a3f5be3a';
 
 // const payload = { user_id: 1, username: 'example_user' };
-// const expiresIn = '1m';
+// const expiresIn = '1d';
 // const token = jwt.sign(payload, SECRET_KEY, { expiresIn });
 
 // console.log(`Bearer ${token}`);
