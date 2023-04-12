71968c3205cf47b1db3ca0c6a35b7d9be5792a23
Xarizz <76038882+Xarizz@users.noreply.github.com>
2023-04-12 21:45:16 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,29 +9,40 @@ const SECRET_KEY = '33595c822eb6d52452e653a39aa109a185642e7d102cf2f300669911a3f5
 
 
 function validateBearerToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) {
-    return res.status(401).json({ error: 'Unauthorized: No token provided' });
-  }
-
-  jwt.verify(token, SECRET_KEY, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ error: 'Forbidden: Invalid token' });
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized: No token provided' });
     }
-    req.user = decoded;
-    next();
-  });
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ error: 'Forbidden: Invalid token' });
+        }
+        req.user = decoded;
+        next();
+    });
 }
 
 
 app.get('/protected', validateBearerToken, (req, res) => {
-  res.json({ message: 'You have access to the protected resource!' });
+    res.json({ message: 'You have access to the protected resource!' });
 });
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+// const jwt = require('jsonwebtoken');
+// const SECRET_KEY = '33595c822eb6d52452e653a39aa109a185642e7d102cf2f300669911a3f5be3a';
+
+// const payload = { user_id: 1, username: 'example_user' };
+// const expiresIn = '1m';
+// const token = jwt.sign(payload, SECRET_KEY, { expiresIn });
+
+// console.log(`Bearer ${token}`);
