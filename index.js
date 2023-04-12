073a6f04beb5e81acf64b32a753c95b5757f46c9
Xarizz <76038882+Xarizz@users.noreply.github.com>
2023-04-13 01:31:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-
+const rateLimit = require('express-rate-limit');
 const app = express();
 app.use(bodyParser.json());
 
 const SECRET_KEY = '33595c822eb6d52452e653a39aa109a185642e7d102cf2f300669911a3f5be3a';
 
 const steamAccs = []
+const windScribeAccs = []
 function validateBearerToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
@@ -26,14 +27,27 @@ function validateBearerToken(req, res, next) {
 }
 app.post('/gensteam', validateBearerToken, (req, res) => {
     const { username, password } = req.query;
-  
+
     if (!username || !password) {
-      return res.status(400).json({ error: 'Bad Request: Missing username or password' });
+        return res.status(400).json({ error: 'Bad Request: Missing username or password' });
     }
-  
+
     steamAccs.push({ username, password });
     res.status(201).json({ message: 'Credentials added to the array' });
-  });
+});
+
+app.post('/genwind', validateBearerToken, (req, res) => {
+    const { username, password } = req.query;
+
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Bad Request: Missing username or password' });
+    }
+
+    windScribeAccs.push({ username, password });
+    res.status(201).json({ message: 'Credentials added to the array' });
+});
+
+
 
 app.get('/steam', (req, res) => {
     if (steamAccs.length == 0) {
@@ -43,6 +57,18 @@ app.get('/steam', (req, res) => {
     const firstCredential = steamAccs.shift();
     res.json(firstCredential);
 });
+
+
+
+app.get('/windscribe', validateBearerToken, (req, res) => {
+    if (windScribeAccs.length == 0) {
+        return res.status(404).json({ error: 'accounts generator reached!' });
+    }
+    const firstCredential = windScribeAccs.shift();
+    res.json(firstCredential);
+});
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
